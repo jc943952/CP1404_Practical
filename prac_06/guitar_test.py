2c9df64b1a2e0f7821066f7ce17e08261d6ed82f
jc943952 <joshua.andrew@my.jcu.edu.au>
2025-11-01 18:08:55 +1000

    Add Guitar class with methods, testing, and main program

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,24 @@
+"""
+Estimate: 60 minutes
+Start time: 5:10 PM
+Actual time:
+
+Tests for the Guitar class methods get_age() and is_vintage().
+"""
+
+from guitar import Guitar
+
+
+def main():
+    """Test the Guitar class."""
+
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    another = Guitar("Another Guitar", 2013, 500)
+
+    print(f"{gibson.name} get_age() - Expected 100. Got {gibson.get_age()}")
+    print(f"{another.name} get_age() - Expected 9. Got {another.get_age()}")
+
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{another.name} is_vintage() - Expected False. Got {another.is_vintage()}")
+
+main()
