ed056fa8449a9a34541f02fe5250ab7226c5c5d3
jc943952 <joshua.andrew@my.jcu.edu.au>
2025-11-30 16:22:02 +1000

    Complete testing solution for prac 10

---
+++ prac_10/testing.py
@@ -0,0 +1,51 @@
+"""
+CP1404/CP5632 Practical
+Testing code using assert and doctest
+"""
+
+import doctest
+from prac_06.car import Car
+
+
+def repeat_string(s, n):
+    """Repeat string s, n times, with spaces in between."""
+    return" ".join([s] * n)
+
+
+def is_long_word(word, length=5):
+    """
+    Determine if the word is as long or longer than the length passed in
+    >>> is_long_word("not")
+    False
+    >>> is_long_word("supercalifrag")
+    True
+    >>> is_long_word("Python", 6)
+    True
+    """
+    return len(word) >= length
+
+def format_sentence(phrase):
+    phrase = phrase.strip()
+    if not phrase.endswith("."):
+        phrase += "."
+    return phrase[0].upper() + phrase[1:]
+
+
+def run_tests():
+    """Run the tests on the functions."""
+    # assert test with no message - used to see if the function works properly
+    assert repeat_string("Python", 1) == "Python"
+    # the test below should fail
+    assert repeat_string("hi", 2) == "hi hi"
+    car = Car()
+    assert car._odometer == 0, "Car does not set odometer correctly"
+
+
+    car = Car(fuel=10)
+    assert car.fuel == 10
+    car_default = Car()
+    assert car_default.fuel == 0
+
+
+run_tests()
+doctest.testmod()
